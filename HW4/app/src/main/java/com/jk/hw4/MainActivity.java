614c8bdf2bd8cd075aecb6c074db781347892842
Jon <jon.king047@gmail.com>
2019-05-11 14:49:18 -0700

    add fragments HW4

--- HW4/app/src/main/java/com/jk/hw4/MainActivity.java
+++ HW4/app/src/main/java/com/jk/hw4/MainActivity.java
@@ -1,13 +1,35 @@
 package com.jk.hw4;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HomeFrag.OnNextClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragMang = getSupportFragmentManager();
+        FragmentTransaction fragTrans = fragMang.beginTransaction();
+        HomeFrag frag = new HomeFrag();
+        fragTrans.commit();
+    }
+
+    public void swapFrags(Fragment frag){
+        Fragment newFrag = null;
+        if (frag instanceof HomeFrag){
+            newFrag = new AboutFrag();
+        }else{
+            newFrag = new HomeFrag();
+        }
+
+        FragmentTransaction fragTrans = getSupportFragmentManager().beginTransaction();
+        fragTrans.replace(R.id.fragment_home, newFrag);
+        fragTrans.addToBackStack(null);
+        fragTrans.commit();
     }
 }
